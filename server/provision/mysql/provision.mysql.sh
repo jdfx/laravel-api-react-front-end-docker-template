af41fee5ce9a6c34ebeef9465064f6d73131c87c
= <=>
2020-10-22 15:12:40 +0100

    mysql provision updates

--- server/provision/mysql/provision.mysql.sh
+++ server/provision/mysql/provision.mysql.sh
@@ -1,4 +1,4 @@
 printf "${GREEN}RUNNING DOCKER ENTRYPOINT SCRIPT: provision.mysql.sh${NC}\n"
 
-mysql -u root -p8shXa+2xW_nrdJe7_1 -e "GRANT ALL PRIVILEGES ON *.* TO root@'%' WITH GRANT OPTION;"
-mysql -u root -p8shXa+2xW_nrdJe7_1 -e "CREATE DATABASE IF NOT EXISTS laravel_api;"
+mysql -u root -pchangeme -e "GRANT ALL PRIVILEGES ON *.* TO root@'%' WITH GRANT OPTION;"
+mysql -u root -pchangeme -e "CREATE DATABASE IF NOT EXISTS laravel_api;"
